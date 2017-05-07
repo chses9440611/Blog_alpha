63ccf0395673cbe6893a9cdd3031926470ba91fe
Jason Huang <chses910372@gmail.com>
2017-05-07 21:57:02 +0800

    Modift text color

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,9 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( pages.css )
+
+
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( edit.css )
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( posts.css )
